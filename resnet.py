a589bbd43d8d426315dd2019047e654b7a65e969
LEEYEONSU <leeyunsoo4229@hotmail.com>
2021-03-06 16:36:01 +0000

    update code

---
+++ resnet.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResNet(nn.Module):
+        def __init__ (self, ):
+                super(ResNet, self).__init__()
+        
+        # layer1, layer2, layer3
+        def make_layers(self, ):
+
+        def forward(self, x):
+
+class ResidualBlock(nn.Module):
+
+        def __init__(self,):
+                super(ResidualBlock, self).__init__()
+
+        def forward(self, x):
+
+# Downsampling Option A
+class IdentityMapping(nn.Module):
+
+        def __init__(self,):
+        	super(IdentityPadding, self).__init__()
+
+        def forward(self, x):
+
--- train.py
+++ train.py
@@ -1,9 +1,20 @@
 import torch
-import torch.nn as nn
+import argparser
 import torchvision
+import torch.nn as nn
 import torchvision.transforms as transforms
 
-import resnet
+
+from resnet import * 
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+
+######################################################
+parser = argparse.ArgumentParser(description='cifar10 classification models')
+
+args = parser.parse_args()
+######################################################
+
 
 # CIFAR-10 training transformation
 transform = transforms.Compose([
@@ -35,7 +46,10 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 # Device Config
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# weight_init? reference 달림 
-# optimizer weight_decay = 1e-4, momentum = 0.9
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(---, lr = 0.1, weight_decay = 1e-4, momentum = 0.9)
+lr_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones = [32000,48000], gamma = 0.1)
+
+# weight_init? reference 달림 kaiming_normal??
 # Epoch = 80, lr = MultiStepLR 
 # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
