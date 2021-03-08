d6e3c1d6dfa9b5bb3f21fb44f5e011b1a332fcea
LEEYEONSU <leeyunsoo4229@hotmail.com>
2021-03-08 05:29:13 +0000

    code cleanup

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+import os 
+import time
 import torch
-import argparser
+import argparse
 import torchvision
 import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
 import torchvision.transforms as transforms
 
-from train import train
-from resnet import Resnet
+from utils.train import *
+from model.resnet import resnet
+from utils.function import *
+from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 
-######################################################
+#. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .#
+
 parser = argparse.ArgumentParser(description = 'Pytorch Resnet-32 model for CIFAR-10 Classification')
+
+parser.add_argument('--print-freq', default=10, type=int, help = 'Print frequency')
+parser.add_argument('--save_dir', default='./save_model/', type=str, help = 'saving model path')
+parser.add_argument('--save-every', dest='save_every',help='Saves checkpoints at every specified number of epochs', type=int, default=10)
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true', help='evaluate model on validation set')
 
 parser.add_argument('--lr', default=0.1, help = 'learning rate')
 parser.add_argument('--weight_decay', default = 1e-4, help = 'weight_decay')
-parser.add_argument('--momentum', default = 0.9 help = 'momentum')
+parser.add_argument('--momentum', default = 0.9, help = 'momentum')
 
 parser.add_argument('--Epoch', default = 80, help = ' Epoch ')
 parser.add_argument('--batch_size', default = 128, help = 'TRAIN batch size ')
@@ -23,4 +35,11 @@ parser.add_argument('--num_worker', default = 4, help = 'Num of workers')
 parser.add_argument('--logdir', type = str, default = 'logs', help = '')
 
 args = parser.parse_args()
-######################################################
+
+#. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .#
+
+
+if __name__ == '__main__':
+    
+        main(args)
+        
--- utils/train.py
+++ utils/train.py
@@ -8,36 +8,18 @@ import torch.optim as optim
 import torch.utils.data
 import torchvision.transforms as transforms
 
-from resnet import resnet
+from model.resnet import resnet
+from utils.function import *
 from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 
-######################################################
-parser = argparse.ArgumentParser(description = 'Pytorch Resnet-32 model for CIFAR-10 Classification')
-
-parser.add_argument('--print-freq', default=30, type=int, help = 'Print frequency')
-parser.add_argument('--save_dir', default='./save_model/', type=str, help = 'saving model path')
-parser.add_argument('--save-every', dest='save_every',help='Saves checkpoints at every specified number of epochs', type=int, default=10)
-parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true', help='evaluate model on validation set')
-
-parser.add_argument('--lr', default=0.1, help = 'learning rate')
-parser.add_argument('--weight_decay', default = 1e-4, help = 'weight_decay')
-parser.add_argument('--momentum', default = 0.9, help = 'momentum')
-
-parser.add_argument('--Epoch', default = 80, help = ' Epoch ')
-parser.add_argument('--batch_size', default = 128, help = 'TRAIN batch size ')
-parser.add_argument('--test_batch_size', default = 100, help = 'TEST batch size')
-parser.add_argument('--num_worker', default = 4, help = 'Num of workers')
-parser.add_argument('--logdir', type = str, default = 'logs', help = '')
-
-args = parser.parse_args()
-######################################################
 
 best_prec1 = 0
-def main():
+def main(args):
 
-        global args, best_prec1
+        global best_prec1
+        
         # CIFAR-10 Training & Test Transformation
         print('. . . . . . . . . . . . . . . .PREPROCESSING DATA . . . . . . . . . . . . . . . .')
         TRAIN_transform = transforms.Compose([
@@ -55,13 +37,13 @@ def main():
 
         # CIFAR-10 dataset
         train_dataset = torchvision.datasets.CIFAR10(root = '../data/', 
-                                                                                train = True, 
-                                                                                transform = TRAIN_transform, 
-                                                                                download = True)
+                                                     train = True, 
+                                                     transform = TRAIN_transform, 
+                                                     download = True)
         val_dataset = torchvision.datasets.CIFAR10(root = '../data/', 
-                                                                                train = False, 
-                                                                                transform = VAL_transform,
-                                                                                download = True)
+                                                     train = False, 
+                                                     transform = VAL_transform,
+                                                     download = True)
 
         # Data loader
         train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
@@ -83,23 +65,23 @@ def main():
         lr_schedule = lr_scheduler.MultiStepLR(optimizer, milestones = [32000,48000], gamma = 0.1)
 
         if args.evaluate:
-                validate(val_loader, model, criterion)
+                validate(val_loader, model, criterion, args)
 
         #  Epoch = args.Epoch
-        for epoch in range(0, args.Epoch):
+        for epoch_ in range(0, args.Epoch):
 
                 print('current lr {:.5e}'.format(optimizer.param_groups[0]['lr']))
-                train(train_loader, model, criterion, optimizer, args.Epoch)
+                train(train_loader, model, criterion, optimizer, args.Epoch, args)
                 lr_schedule.step()
 
-                prec1 = validation(val_loader, model, criterion)
+                prec1 = validation(val_loader, model, criterion, args)
 
                 is_best = prec1 > best_prec1
                 best_prec1 = max(prec1, best_prec1)
 
-                if epoch > 0 and epoch % args.save_every == 0:
+                if epoch_ > 0 and epoch_ % args.save_every == 0:
                         save_checkpoint({
-                        'epoch': epoch + 1,
+                        'epoch': epoch_ + 1,
                         'state_dict': model.state_dict(),
                         'best_prec1': best_prec1,
                         }, is_best, filename=os.path.join(args.save_dir, 'checkpoint.th'))
@@ -109,7 +91,7 @@ def main():
                 'best_prec1': best_prec1,
                 }, is_best, filename=os.path.join(args.save_dir, 'model.th'))
 
-def train(train_loader, model, criterion, optimizer, epoch):
+def train(train_loader, model, criterion, optimizer, epoch_, args):
         
         batch_time = AverageMeter()
         data_time = AverageMeter()
@@ -137,6 +119,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
                 output = output.float()
                 loss = loss.float()
+            
                 # measure accuracy and record loss
                 prec1 = accuracy(output.data, target)[0]
                 losses.update(loss.item(), input_.size(0))
@@ -149,13 +132,13 @@ def train(train_loader, model, criterion, optimizer, epoch):
                 if i % args.print_freq == 0:
                         print('Epoch: [{0}][{1}/{2}]\t'
                                 'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                                'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+#                                 'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                                 'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                                 'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
-                                epoch, i, len(train_loader), batch_time=batch_time,
+                                epoch_, i, len(train_loader), batch_time=batch_time,
                                 data_time=data_time, loss=losses, top1=top1))
 
-def validation(val_loader, model, criterion):
+def validation(val_loader, model, criterion, args):
 
         batch_time = AverageMeter()
         losses = AverageMeter()
@@ -195,46 +178,3 @@ def validation(val_loader, model, criterion):
                 print(' * Prec@1 {top1.avg:.3f}'.format(top1=top1))
 
         return top1.avg
-
-### CODE FROM https://github.com/akamaster/pytorch_resnet_cifar10/blob/master/trainer.py
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
-
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    """
-    Save the training model
-    """
-    torch.save(state, filename)
-
-if __name__ == '__main__':
-        main()
-        
